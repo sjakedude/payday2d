66cbb59a7e2751e8a1f45f49dc60b2cfe33d5273
Jake Stephens <sjakedude@gmail.com>
2022-02-03 14:49:08 -0500

    fixed bug with level loader flipping the axis

--- parse_level.py
+++ parse_level.py
@@ -23,8 +23,8 @@ class LevelParser:
                 if item == "1":
                     rect = pygame.Rect(column * self.column_factor, row * self.row_factor, self.column_factor, self.row_factor)
                     rects.append(rect)
-                row += 1
-            row = 0
-            column += 1
+                column += 1
+            column = 0
+            row += 1
         return rects
 
