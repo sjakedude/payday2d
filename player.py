67b2af43facdc9bda519b2a2eec0a67923132ff5
Jake Stephens <sjakedude@gmail.com>
2022-02-09 12:52:51 -0500

    changing size of player

--- player.py
+++ player.py
@@ -6,10 +6,10 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.image = pygame.transform.scale(
-            pygame.image.load("assets/player.png"), (50, 50)
+            pygame.image.load("assets/player.png"), (31, 31)
         )
         self.image_original = pygame.transform.scale(
-            pygame.image.load("assets/player.png"), (50, 50)
+            pygame.image.load("assets/player.png"), (31, 31)
         )
         self.rect = self.image.get_rect(topleft=pos)
         self.position = pos
